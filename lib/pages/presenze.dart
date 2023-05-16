f84332d43784e9680f57fbedfb024765b20045ca
lorenzo-vecchio <106695602+lorenzo-vecchio@users.noreply.github.com>
2023-05-16 12:40:58 +0200

    sincro

--- lib/pages/presenze.dart
+++ lib/pages/presenze.dart
@@ -18,6 +18,57 @@ class Presenze extends StatefulWidget {
 }
 
 class _PresenzeState extends State<Presenze> {
+  List<String> listaNomiMaterie = [];
+  List<PresenzaAssenza> listaMaterie = globalData.presenzeList;
+  List<String> selectedFilters = [];
+
+  double _getPercentPresenze(List<PresenzaAssenza> presenzeList) {
+    int minutiP = 0;
+    int minutiA = 0;
+    for (PresenzaAssenza item in presenzeList) {
+      minutiP = minutiP + item.minuti_presenza + (item.ore_presenza * 60);
+      minutiA = minutiA + item.minuti_assenza + (item.ore_assenza * 60);
+    }
+    int totale = minutiP + minutiA;
+    double percentuale = minutiP * 100 / totale;
+    return percentuale;
+  }
+
+  List<String> _getNomiMaterie() {
+    List<String> risultato = [];
+    for (PresenzaAssenza materia in globalData.presenzeList) {
+      String titolo = materia.materia;
+      if (!risultato.contains(titolo)) {
+        risultato.add(titolo);
+      }
+    }
+    return risultato;
+  }
+
+  void _selezioneMaterie(List<dynamic> value) {
+    List<String> selectedItems = value.cast<String>();
+    selectedFilters = selectedItems;
+    List<PresenzaAssenza> filteredList = [];
+    if (selectedItems.length == 0) {
+      listaMaterie = globalData.presenzeList;
+      setState(() {});
+      return;
+    }
+    for (PresenzaAssenza item in globalData.presenzeList) {
+      if (selectedItems.contains(item.materia)) {
+        filteredList.add(item);
+      }
+    }
+    listaMaterie = filteredList;
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    listaNomiMaterie = _getNomiMaterie();
+  }
+
   @override
   void didChangeDependencies() {
     super.didChangeDependencies();
@@ -29,163 +80,159 @@ class _PresenzeState extends State<Presenze> {
     // gets if it's in dark mode or not
     return Consumer<ThemeModel>(builder: (context, model, child) {
       return Scaffold(
-        body: ListView.builder(
-          itemCount: globalData.presenzeList.length + 2,
-          itemBuilder: (context, index) {
-            if (index >= 2) {
-              PresenzaAssenza presenza = globalData.presenzeList[index - 2];
-              return Padding(
-                padding: EdgeInsets.fromLTRB(ScreenSize.padding10,
-                    ScreenSize.padding10, ScreenSize.padding10, 0),
-                child: ListTile(
-                  title: Text(
-                    presenza.materia,
-                    style: const TextStyle(
-                      fontSize: 15,
+      backgroundColor: model.isDarkMode ? Colors.black : Colors.white,
+      body: ListView.builder(
+        itemCount: listaMaterie.length + 2,
+        itemBuilder: (context, index) {
+          if (index >= 2) {
+            PresenzaAssenza presenza = listaMaterie[index - 2];
+            return Padding(
+              padding: EdgeInsets.fromLTRB(ScreenSize.padding10,
+                  ScreenSize.padding10, ScreenSize.padding10, 0),
+              child: ListTile(
+                title: Text(
+                  presenza.materia,
+                  style: const TextStyle(
+                    fontSize: 15,
+                  ),
+                ),
+                subtitle: Text(
+                    '${DateFormat('dd/MM/yyyy').format(presenza.data)}  -  ${DateFormat('HH:mm').format(presenza.inizio)} | ${DateFormat('HH:mm').format(presenza.fine)}'),
+                trailing: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Container(
+                      padding: EdgeInsets.symmetric(
+                          horizontal: ScreenSize.padding10,
+                          vertical: ScreenSize.padding8),
+                      decoration: BoxDecoration(
+                        color:
+                            model.isDarkMode ? Colors.green : Colors.green.shade500,
+                        borderRadius: BorderRadius.circular(5),
+                      ),
+                      child: Text(
+                        '${presenza.ore_presenza}h ${presenza.minuti_presenza}m',
+                        style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
                     ),
-                  ),
-                  subtitle: Text(
-                      '${DateFormat('dd/MM/yyyy').format(presenza.data)}  -  ${DateFormat('HH:mm').format(presenza.inizio)} | ${DateFormat('HH:mm').format(presenza.fine)}'),
-                  trailing: Row(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      Container(
-                        padding: EdgeInsets.symmetric(
-                            horizontal: ScreenSize.padding10,
-                            vertical: ScreenSize.padding8),
-                        decoration: BoxDecoration(
-                          color:
-                              model.isDarkMode ? Colors.green : Colors.green.shade500,
-                          borderRadius: BorderRadius.circular(5),
-                        ),
-                        child: Text(
-                          '${presenza.ore_presenza}h',
-                          style: const TextStyle(
-                            fontWeight: FontWeight.bold,
-                          ),
+                    const SizedBox(width: 8),
+                    Container(
+                      padding: EdgeInsets.symmetric(
+                          horizontal: ScreenSize.padding10,
+                          vertical: ScreenSize.padding8),
+                      decoration: BoxDecoration(
+                        color: model.isDarkMode ? Colors.red : Colors.red.shade400,
+                        borderRadius: BorderRadius.circular(5),
+                      ),
+                      child: Text(
+                        '${presenza.ore_assenza}h ${presenza.minuti_assenza}m',
+                        style: const TextStyle(
+                          fontWeight: FontWeight.bold,
                         ),
                       ),
-                      const SizedBox(width: 8),
-                      Container(
-                        padding: EdgeInsets.symmetric(
-                            horizontal: ScreenSize.padding10,
-                            vertical: ScreenSize.padding8),
-                        decoration: BoxDecoration(
-                          color: model.isDarkMode ? Colors.red : Colors.red.shade400,
-                          borderRadius: BorderRadius.circular(5),
-                        ),
-                        child: Text(
-                          '${presenza.ore_assenza}h',
-                          style: const TextStyle(
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
-              );
-            }
-            if (index == 0) {
-              return Padding(
-                padding: EdgeInsets.fromLTRB(ScreenSize.padding10,
-                    ScreenSize.padding10, ScreenSize.padding10, 0),
-                child: Container(
-                  height: 150,
-                  padding:
-                      EdgeInsets.symmetric(horizontal: ScreenSize.padding30),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(15),
-                    color: model.isDarkMode
-                        ? Colors.grey.shade900.withOpacity(0.50)
-                        : Colors.grey.shade400.withOpacity(0.50),
-                  ),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Container(
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(15),
-                          color:
-                              model.isDarkMode ? Colors.green : Colors.green.shade500,
-                        ),
-                        height: 90,
-                        alignment: Alignment.center,
-                        padding: EdgeInsets.symmetric(
-                            horizontal: ScreenSize.padding30),
-                        child: RichText(
-                          textAlign: TextAlign.center,
-                          text: TextSpan(
-                            text: 'Presenza:\n',
-                            style: TextStyle(
-                              color: model.isDarkMode ? Colors.white : Colors.black,
-                              fontWeight: FontWeight.bold,
-                              fontSize: 17,
-                            ),
-                            children: [
-                              TextSpan(
-                                text:
-                                    '${globalData.sommaPresenzeAssenze['tot_ore_presenza']}h ${globalData.sommaPresenzeAssenze['tot_min_presenza']}m',
-                                style: TextStyle(
-                                  color:
-                                      model.isDarkMode ? Colors.white : Colors.black,
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 20,
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
-                      Container(
-                        height: 90,
-                        alignment: Alignment.center,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(15),
-                          color: model.isDarkMode ? Colors.red : Colors.red.shade400,
-                        ),
-                        padding: EdgeInsets.symmetric(
-                            horizontal: ScreenSize.padding30),
-                        child: RichText(
-                          textAlign: TextAlign.center,
-                          text: TextSpan(
-                            text: 'Assenza:\n',
-                            style: TextStyle(
-                              color: model.isDarkMode ? Colors.white : Colors.black,
-                              fontWeight: FontWeight.bold,
-                              fontSize: 17,
-                            ),
-                            children: [
-                              TextSpan(
-                                text:
-                                    '${globalData.sommaPresenzeAssenze['tot_ore_assenza']}h ${globalData.sommaPresenzeAssenze['tot_min_assenza']}m',
-                                style: TextStyle(
-                                  color:
-                                      model.isDarkMode ? Colors.white : Colors.black,
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 20,
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
+              ),
+            );
+          }
+          if (index == 0) {
+            return Padding(
+              padding: EdgeInsets.fromLTRB(ScreenSize.padding10,
+                  ScreenSize.padding10, ScreenSize.padding10, 0),
+              child: Container(
+                height: 150,
+                padding: EdgeInsets.symmetric(horizontal: ScreenSize.padding30),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
+                  color: model.isDarkMode
+                      ? Colors.grey.shade900.withOpacity(0.50)
+                      : Colors.grey.shade400.withOpacity(0.50),
                 ),
-              );
-            }
-            if (index == 1) {
-              return Column(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15),
+                        color:
+                            model.isDarkMode ? Colors.green : Colors.green.shade500,
+                      ),
+                      height: 90,
+                      width: ScreenSize.screenWidth * 0.36,
+                      alignment: Alignment.center,
+                      child: RichText(
+                        textAlign: TextAlign.center,
+                        text: TextSpan(
+                          text: 'Presenza:\n',
+                          style: TextStyle(
+                            color: model.isDarkMode ? Colors.white : Colors.black,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 17,
+                          ),
+                          children: [
+                            TextSpan(
+                              text:
+                                  '${globalData.sommaPresenzeAssenze['tot_ore_presenza']}h ${globalData.sommaPresenzeAssenze['tot_min_presenza']}m',
+                              style: TextStyle(
+                                color: model.isDarkMode ? Colors.white : Colors.black,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 20,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    Container(
+                      height: 90,
+                      width: ScreenSize.screenWidth * 0.36,
+                      alignment: Alignment.center,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15),
+                        color: model.isDarkMode ? Colors.red : Colors.red.shade400,
+                      ),
+                      child: RichText(
+                        textAlign: TextAlign.center,
+                        text: TextSpan(
+                          text: 'Assenza:\n',
+                          style: TextStyle(
+                            color: model.isDarkMode ? Colors.white : Colors.black,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 17,
+                          ),
+                          children: [
+                            TextSpan(
+                              text:
+                                  '${globalData.sommaPresenzeAssenze['tot_ore_assenza']}h ${globalData.sommaPresenzeAssenze['tot_min_assenza']}m',
+                              style: TextStyle(
+                                color: model.isDarkMode ? Colors.white : Colors.black,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 20,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            );
+          }
+          if (index == 1) {
+            return Column(
               children: [
                 Padding(
                   padding: EdgeInsets.fromLTRB(ScreenSize.screenWidth * 0.05,
                       ScreenSize.padding8, ScreenSize.padding8, 0),
                   child: ListTile(
-                    tileColor: isDarkMode ? Colors.black : Colors.white,
+                    tileColor: model.isDarkMode ? Colors.black : Colors.white,
                     shape: RoundedRectangleBorder(
                       side: BorderSide(
-                        color: isDarkMode ? Colors.black : Colors.white,
+                        color: model.isDarkMode ? Colors.black : Colors.white,
                       ),
                     ),
                     title: const Text(
@@ -211,11 +258,11 @@ class _PresenzeState extends State<Presenze> {
                             onApply: _selezioneMaterie,
                             selectedItems: selectedFilters,
                             backgroundColor:
-                                isDarkMode ? Colors.black : Colors.white,
+                                model.isDarkMode ? Colors.black : Colors.white,
                             activeColor: Colors.red,
                             notActiveColor:
-                                isDarkMode ? Colors.white : Colors.black,
-                            tileBackgroundColor: isDarkMode
+                                model.isDarkMode ? Colors.white : Colors.black,
+                            tileBackgroundColor: model.isDarkMode
                                 ? Colors.grey.shade900.withOpacity(0.50)
                                 : Colors.grey.shade300.withOpacity(0.50),
                             buttonColor: Colors.red,
@@ -242,7 +289,7 @@ class _PresenzeState extends State<Presenze> {
                         : selectedFilters
                             .map((String item) => Container(
                                 decoration: BoxDecoration(
-                                    color: isDarkMode
+                                    color: model.isDarkMode
                                         ? Colors.grey.shade900.withOpacity(0.50)
                                         : Colors.grey.shade300
                                             .withOpacity(0.50),
@@ -253,7 +300,7 @@ class _PresenzeState extends State<Presenze> {
                                   child: Text(
                                     item,
                                     style: TextStyle(
-                                        color: isDarkMode
+                                        color: model.isDarkMode
                                             ? Colors.white
                                             : Colors.black,
                                         fontSize: 13),
@@ -282,7 +329,7 @@ class _PresenzeState extends State<Presenze> {
                         width: ScreenSize.screenWidth * 0.8,
                         lineHeight: 23.0,
                         percent: _getPercentPresenze(listaMaterie) / 100,
-                        backgroundColor: isDarkMode
+                        backgroundColor: model.isDarkMode
                             ? Colors.grey.shade900.withOpacity(0.50)
                             : Colors.grey.shade300.withOpacity(0.50),
                         progressColor: Colors.red,
@@ -290,7 +337,7 @@ class _PresenzeState extends State<Presenze> {
                           '${_getPercentPresenze(listaMaterie).toStringAsFixed(2)}%',
                           style: TextStyle(
                               fontWeight: FontWeight.bold,
-                              color: isDarkMode ? Colors.white : Colors.black),
+                              color: model.isDarkMode ? Colors.white : Colors.black),
                         ),
                       ),
                     ],
@@ -298,9 +345,10 @@ class _PresenzeState extends State<Presenze> {
                 ),
               ],
             );
-          },
-        ),
-      );
+          }
+        },
+      ),
+    );
     });
   }
 }
